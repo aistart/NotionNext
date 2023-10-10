2318c2b93314746f0eaedd19df4cff19535aeea4
aistart <aistart@aliyun.com>
2023-10-10 22:50:41 +0800

    增加about

--- blog.config.js
+++ blog.config.js
@@ -19,7 +19,7 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'アルファ未来株式会社', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '华人投资日本房产的首选顾问', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://amirai.daoo.tech', // 网站地址
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://amirai.daoo.tech/about', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '日本房产, 房产投资, 置业顾问, 房产中介', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
