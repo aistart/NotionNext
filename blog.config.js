2f1d1cb4c6c90e1fc1f5167f9b589e463a32609f
aistart <aistart@aliyun.com>
2023-10-15 10:53:18 +0800

    weibo

--- blog.config.js
+++ blog.config.js
@@ -17,14 +17,14 @@ const BLOG = {
 
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
-  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'アルファ未来株式会社', // 您的昵称 例如 tangly1024
+  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'アルファ未来株式会社', // 您的昵称 例如 tangly1024github
   BIO: process.env.NEXT_PUBLIC_BIO || '华人投资日本房产的首选顾问', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://amirai.daoo.tech/about', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '日本房产, 房产投资, 置业顾问, 房产中介', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || '', // 邮箱地址 例如mail@tangly1024.com
-  CONTACT_WEIBO: process.env.NEXT_PUBLIC_CONTACT_WEIBO || '', // 你的微博个人主页
+  CONTACT_WEIBO: process.env.NEXT_PUBLIC_CONTACT_WEIBO || 'https://work.weixin.qq.com/kfid/kfcb8f0c45497ca5b6a', // 你的微博个人主页
   CONTACT_TWITTER: process.env.NEXT_PUBLIC_CONTACT_TWITTER || '', // 你的twitter个人主页
   CONTACT_GITHUB: process.env.NEXT_PUBLIC_CONTACT_GITHUB || '', // 你的github个人主页 例如 https://github.com/tangly1024
   CONTACT_TELEGRAM: process.env.NEXT_PUBLIC_CONTACT_TELEGRAM || '', // 你的telegram 地址 例如 https://t.me/tangly_1024
