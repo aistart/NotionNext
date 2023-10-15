d08cd1eb86d0086283a5c23137fecff6805236d8
aistart <aistart@aliyun.com>
2023-10-15 17:41:20 +0800

    shareUrl显示

--- components/ShareButtons.js
+++ components/ShareButtons.js
@@ -64,7 +64,7 @@ const ShareButtons = ({ shareUrl, title, body, image }) => {
 
   const copyUrl = () => {
     copy(shareUrl)
-    alert(locale.COMMON.URL_COPIED)
+    alert(shareUrl + locale.COMMON.URL_COPIED)
   }
 
   const openPopover = () => {
@@ -360,7 +360,7 @@ const ShareButtons = ({ shareUrl, title, body, image }) => {
         }
         if (singleService === 'wechat') {
           return <button key={singleService} className='cursor-pointer bg-green-600 text-white rounded-full mx-1'>
-            <a target='_blank' rel='noreferrer' href={'https://work.weixin.qq.com/kfid/kfcb8f0c45497ca5b6a'} >
+            <a target='_blank' rel='noreferrer' href={CONTACT_WEIXIN_E} >
               <i className='fab fa-weixin w-8' />
             </a>
           </button>
